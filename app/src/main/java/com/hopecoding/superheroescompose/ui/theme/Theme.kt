fcade07a1f03e0ebe1aef69a85471ccf9dc38c4f
Umut Can Aldırmaz <ucan.aldirmaz@gmail.com>
2025-05-03 17:08:46 +0300

    Implement Material Design 3 theming
    This commit implements Material Design 3 theming by setting up light and dark color schemes. It also removes unused classes and variables related to Material Design 2 and it customizes the app theme using Material Design 3's `lightColorScheme` and `darkColorScheme` to handle light and dark modes respectively, dynamically adjusting based on the system's settings.

--- app/src/main/java/com/hopecoding/superheroescompose/ui/theme/Theme.kt
+++ app/src/main/java/com/hopecoding/superheroescompose/ui/theme/Theme.kt
@@ -4,13 +4,11 @@ import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Shapes
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.Immutable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
@@ -94,19 +92,6 @@ private val darkScheme = darkColorScheme(
     surfaceContainerHighest = surfaceContainerHighestDark,
 )
 
-
-
-@Immutable
-data class ColorFamily(
-    val color: Color,
-    val onColor: Color,
-    val colorContainer: Color,
-    val onColorContainer: Color
-)
-
-val unspecified_scheme = ColorFamily(
-    Color.Unspecified, Color.Unspecified, Color.Unspecified, Color.Unspecified
-)
 
 @Composable
 fun AppTheme(
