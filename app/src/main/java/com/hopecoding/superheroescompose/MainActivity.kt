ab10dbdc19758f5a8c1db9d5269137b735061a8e
Umut Can Aldırmaz <ucan.aldirmaz@gmail.com>
2025-05-03 16:57:30 +0300

    Add Scaffold and TopAppBar for SuperheroesApp
    This commit introduces a `Scaffold` to the `SuperheroesApp` composable, and a `TopAppBar`. The `TopAppBar` now displays the app's name from string resources. Additionally, the `HeroesRepository` is used to provide heroes data to the `HeroesList`, which will be implemented in the next commits. The `Greeting` function has been removed.

--- app/src/main/java/com/hopecoding/superheroescompose/MainActivity.kt
+++ app/src/main/java/com/hopecoding/superheroescompose/MainActivity.kt
@@ -5,12 +5,17 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import com.hopecoding.superheroescompose.model.HeroesRepository
 import com.hopecoding.superheroescompose.ui.theme.AppTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,11 +24,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             AppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface {
+                    SuperheroesApp()
                 }
             }
         }
@@ -31,17 +33,41 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun SuperheroesApp() {
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        topBar = {
+            TopAppBar()
+        }
+    ) {
+        /* Important: It is not a good practice to access data source directly from the UI.
+        In later units you will learn how to use ViewModel in such scenarios that takes the
+        data source as a dependency and exposes heroes.
+         */
+        val heroes = HeroesRepository.heroes
+        HeroesList(heroes = heroes, contentPadding = it)
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopAppBar(modifier: Modifier = Modifier) {
+    CenterAlignedTopAppBar(
+        title = {
+            Text(
+                text = stringResource(R.string.app_name),
+                style = MaterialTheme.typography.displayLarge,
+            )
+        },
         modifier = modifier
     )
 }
+
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     AppTheme {
-        Greeting("Android")
+        SuperheroesApp()
     }
 }
