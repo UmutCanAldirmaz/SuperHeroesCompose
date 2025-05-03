c826421a1e888d53169fa8975e0c208ad96861e2
Umut Can Aldırmaz <ucan.aldirmaz@gmail.com>
2025-05-03 16:53:45 +0300

    Refactor Hero model to use resource annotations
    This commit refactors the `Hero` data class to use `@StringRes` and `@DrawableRes` annotations for its properties. This ensures that the name, description, and image are always references to string and drawable resources, respectively.

--- app/src/main/java/com/hopecoding/superheroescompose/model/Hero.kt
+++ app/src/main/java/com/hopecoding/superheroescompose/model/Hero.kt
@@ -1,8 +1,11 @@
 package com.hopecoding.superheroescompose.model
 
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
 data class Hero(
-    val nameRes : Int,
-    val descriptionRes : Int,
-    val imageRes : Int,
+    @StringRes val nameRes: Int,
+    @StringRes val descriptionRes: Int,
+    @DrawableRes val imageRes: Int,
 )
 
